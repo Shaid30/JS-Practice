be0c2db74b8ae0297cf0b0a94493186e63c695a6
Abu Shaid <mdabushaid847@gmail.com>
2026-07-24 12:09:50 +0600

    update something

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
  function myFunction() {
     document.getElementById("cng").innerHTML="Paragraph changed";
-    
+
  }
     
+ document.write(5+6);
+ window.alert("Hello Mr. Abu shaid")
+ console.log("hello")
